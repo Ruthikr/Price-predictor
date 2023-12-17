fe0ea8e0e7829e946c0da196c5ac83337caa164b
Ruthikr <147206849+Ruthikr@users.noreply.github.com>
2023-12-17 16:46:31 +0530

    Update price_predictor.py

--- price_predictor.py
+++ price_predictor.py
@@ -11,7 +11,6 @@ with st.form("my_form"):
        'Realme', 'Motorola', 'Nokia', 'Sony', 'LG', 'Asus', 'Blackberry',
        'CAT', 'Huawei'))
 
-st.divider() 
 
   
 
@@ -20,29 +19,29 @@ st.divider()
     (32,64,128,256,512))
 
 
-    st.divider()
+    
 
     ram=st.selectbox(
     'select ram of the phone',
     ( 2,4,6,  8, 12, 16))
 
 
-    st.divider()
+    
 
     screen_size= st.slider('Scree size in "inches"',min_value=1.0,max_value=10.0,value=5.0,step=0.1)
 
 
-    st.divider()
+    
 
     battery=st.slider('battery in "mah"',min_value=1000,max_value=10000,value=5000,step=500)
 
-    st.divider()
+    
 
     first=st.number_input("enter mp of first camera",value=0)
     second=st.number_input("enter mp of second  camera",value=0)
     
 
-    st.divider()
+    
 
     user_data=np.array([[brand,storage,ram,screen_size,battery,first,second,0]])
     result=model.predict(user_data)
