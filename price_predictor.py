5d0cecf1bd86902453c2eccced800353761dc5a6
Ruthik <147206849+Ruthikr@users.noreply.github.com>
2023-12-19 14:42:18 +0530

    Update price_predictor.py

--- price_predictor.py
+++ price_predictor.py
@@ -12,16 +12,16 @@ def convert_to_words(num):
     words = ""  
     if num>= 1000:  
         words += ones[num // 1000] + " thousand "  
-num %= 1000  
+        num %= 1000  
     if num>= 100:  
         words += ones[num // 100] + " hundred "  
-num %= 100  
+        num %= 100  
     if num>= 10 and num<= 19:  
         words += teens[num - 10] + " "  
-num = 0  
-elifnum>= 20:  
+        num = 0  
+    elif num>= 20:  
         words += tens[num // 10] + " "  
-num %= 10  
+        num %= 10  
     if num>= 1 and num<= 9:  
         words += ones[num] + " "  
     return words.strip()
