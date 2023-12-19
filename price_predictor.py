7baccd98538a82565d602d4d9280457810f95a52
Ruthik <147206849+Ruthikr@users.noreply.github.com>
2023-12-19 14:41:05 +0530

    Update price_predictor.py

--- price_predictor.py
+++ price_predictor.py
@@ -2,6 +2,31 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+
+def convert_to_words(num):  
+    if num == 0:  
+        return "zero"  
+    ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]  
+    tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]  
+    teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]  
+    words = ""  
+    if num>= 1000:  
+        words += ones[num // 1000] + " thousand "  
+num %= 1000  
+    if num>= 100:  
+        words += ones[num // 100] + " hundred "  
+num %= 100  
+    if num>= 10 and num<= 19:  
+        words += teens[num - 10] + " "  
+num = 0  
+elifnum>= 20:  
+        words += tens[num // 10] + " "  
+num %= 10  
+    if num>= 1 and num<= 9:  
+        words += ones[num] + " "  
+    return words.strip()
+
+
 model=pickle.load(open("price_pipe_update.pkl","rb"))
 st.title("Phone Price Predictor")
 with st.form("my_form"):
@@ -49,6 +74,7 @@ with st.form("my_form"):
     submitted = st.form_submit_button("Predict")
 if submitted:
         st.title("Estimated price : {} inr".format(round(result[0]*72)))
-        
+        x=round(result[0]*72)
+        st.title(convert_to_words(x))
     
     
