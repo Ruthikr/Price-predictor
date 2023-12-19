2f52922aab97546a3aa82f1c9faeeea3e680cadd
Ruthik <147206849+Ruthikr@users.noreply.github.com>
2023-12-19 15:05:27 +0530

    Update price_predictor.py

--- price_predictor.py
+++ price_predictor.py
@@ -52,6 +52,6 @@ with st.form("my_form"):
     submitted = st.form_submit_button("Predict")
 if submitted:
         st.title("Estimated price : {} inr".format(round(result[0]*80)))
-        x=round(result[0]*72)
-        y=num2words(x)
-        st.write(f"{y.upper()} RUPEES")
+        x=num2words(round(result[0]*72))
+       
+        st.write(f"{x.upper()} RUPEES")
